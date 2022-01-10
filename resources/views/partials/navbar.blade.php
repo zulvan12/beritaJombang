604416e5a7f93143f11230817bdbc443e64528c0
zulvanF <zulvanfahmi1612@gmail.com>
2022-01-10 12:42:02 +0700

    commit ke-2, buat halaman home, file layouts, buat routes ke index

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+      <a class="navbar-brand" href="/">Berita Jombang</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link {{ ($pageTitle === 'Berita Jombang' ? 'active' : '') }}" aria-current="page" href="/">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
