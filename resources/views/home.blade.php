bb1d4ad552d38a49803a13b5ed7b00045f13a498
zulvanF <zulvanfahmi1612@gmail.com>
2022-01-19 10:14:46 +0700

    mengubah halaman home : punya jumbotron dan setiap berita berada di card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="card-body text-center">
       <h3 class="card-title">{{ $news[0]->title }}</h3>
 
-      <p>Category : <a class="text-decoration-none" href="/categories/{{ $news[0]->category->slug }}">{{ $news[0]->category->name }}</a> | Author : <a class="text-decoration-none" href="/authors/{{ $news[0]->author->username }}">{{ $news[0]->author->name }}</a></p>
+      <p class="text-muted">Category : <a class="text-decoration-none" href="/categories/{{ $news[0]->category->slug }}">{{ $news[0]->category->name }}</a> | Author : <a class="text-decoration-none" href="/authors/{{ $news[0]->author->username }}">{{ $news[0]->author->name }}</a></p>
       <p class="card-text">{{ $news[0]->excerpt }}</p>
 
       <p class="card-text"><small class="text-muted">{{ $news[0]->created_at->diffForHumans() }}</small></p>
@@ -23,8 +23,31 @@
     <p class="text-center fs-4">No Post Found</p>
 @endif
 
-@foreach ($news as $singleNews)
-    <article class="mb-2 border-bottom pb-4">
+<div class="row">
+    @foreach ($news->skip(1) as $singleNews)
+        <div class="col-md-4 mb-2">
+            <div class="card">
+                <div class="position-absolute px-3 py-1 bg-dark">
+                    <a class="text-decoration-none text-white" href="/categories/{{ $singleNews->category->slug }}">{{ $singleNews->category->name }}</a>
+                </div>
+
+                <img src="https://source.unsplash.com/300x200?{{ $singleNews->category->en_name }}" class="card-img-top" alt="{{ $singleNews->category->en_name }}">
+
+                <div class="card-body">
+                <h5 class="card-title"><a href="/{{ $singleNews->slug }}" class="text-decoration-none">{{ $singleNews->title }}</a></h5>
+                <small>
+                    <p class="text-muted mb-2">Author : <a class="text-decoration-none" href="/authors/{{ $singleNews->author->username }}">{{ $singleNews->author->name }}</a> | {{ $singleNews->created_at->diffForHumans() }}</p>
+                </small>
+                <p class="card-text">{{ $singleNews->excerpt }}</p>
+                <a href="/{{ $singleNews->slug }}" class="text-decoration-none btn btn-primary">Read More...</a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+
+    {{-- <article class="mb-2 border-bottom pb-4">
         <h2><a href="/{{ $singleNews->slug }}" class="text-decoration-none">{{ $singleNews->title }}</a></h2>
 
         <p>Category : <a class="text-decoration-none" href="/categories/{{ $singleNews->category->slug }}">{{ $singleNews->category->name }}</a> | Author : <a class="text-decoration-none" href="/authors/{{ $singleNews->author->username }}">{{ $singleNews->author->name }}</a> | {{ $singleNews->created_at->diffForHumans() }}</p>
@@ -32,6 +55,6 @@
         <p>{{ $singleNews->excerpt }}</p>
 
         <a href="/{{ $singleNews->slug }}" class="text-decoration-none btn btn-primary">Read More...</a>
-    </article>
-@endforeach
+    </article> --}}
+
 @endsection
